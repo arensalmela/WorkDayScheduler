d3a831ed9beb7352ea51f24d0cb689f45fcea549
Aren S <adsalmela@gmail.com>
2020-10-21 19:27:52 -0400

    Added remaining div tags for the rest of the time blocks.

--- Develop/script.js
+++ Develop/script.js
@@ -1,12 +1,11 @@
 $(document).ready(function(){
 
 $(".saveBtn").on("click",function(){   //Share the same row
-    var schedInput = $(this).siblings(".userInput").val()  //On click button find sibling with class "userInput"
-    var selectedHour = $(this).parent().attr("id") //Find parent element of saveBtn, get value of id attribute
-    localStorage.setItem(selectedHour, schedInput) //Local storage reads time and user input
+    var schedInput = $(this).siblings(".userInput").val()  //On click button find sibling with class "userInput" 
+    var selectedHour = $(this).parent().attr("id") //Find parent element of saveBtn, get value of id attribute (the hour block 9am)
+    localStorage.setItem(selectedHour, schedInput) //Local storage reads time through id and user input
     console.log("this function")
 })
-
     
 
 $("#currentDay").text(moment().format("MMMM Do YYYY")); //Displaying current day
