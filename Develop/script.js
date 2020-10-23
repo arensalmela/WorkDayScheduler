ffbc800fe2af682b05433b1b36d196d357bbf11d
Aren S <adsalmela@gmail.com>
2020-10-22 22:07:34 -0400

    Created timeBlockUpdate function and
    cd ..

--- Develop/script.js
+++ Develop/script.js
@@ -1,13 +1,44 @@
 $(document).ready(function(){
 
-$(".saveBtn").on("click",function(){   //Share the same row
-    var schedInput = $(this).siblings(".userInput").val()  //On click button find sibling with class "userInput" 
-    var selectedHour = $(this).parent().attr("id") //Find parent element of saveBtn, get value of id attribute (the hour block 9am)
-    localStorage.setItem(selectedHour, schedInput) //Local storage reads time through id and user input
+$(".saveBtn").on("click",function(){   
+    //On click button find sibling with class "userInput"
+    var schedInput = $(this).siblings(".userInput").val() 
+    //Find parent element of saveBtn, get value of id attribute (the hour block 9am) 
+    var selectedHour = $(this).parent().attr("id") 
+    //Local storage reads time through id and user input
+    localStorage.setItem(selectedHour, schedInput) 
     console.log("this function")
 })
-    
 
-$("#currentDay").text(moment().format("MMMM Do YYYY")); //Displaying current day
+function timeBlockUpdate(){
+    //currentHour is set to current hour number
+    var currentHour = moment().hours(); 
+    $(".time-block").each(function(){
+    //Variable is set to grab hour number. Index 1 of id is the hour number using split.
+    var hourBlock = parseInt($(this).attr("id").split("-")[1])
+
+    //Adding and removing css class with if statement
+    if (hourBlock < currentHour){
+        $(this).addClass("past") 
+    }
+    else if (hourBlock === currentHour){
+        $(this).addClass("present") 
+        $(this).removeClass("past")
+    }
+    else {
+        $(this).addClass("future")
+        $(this).removeClass("past")
+        $(this).removeClass("present")
+    }
+    })
+    }
+
+    timeBlockUpdate()
+    var hourInterval = setInterval(timeBlockUpdate(), 10000) // Interval to check if current time needs updating
+    $("#hour-8 .userInput").val(localStorage.getItem("hour-9")) //Repeat for every hour
+
+    
+    //Displaying current day
+$("#currentDay").text(moment().format("MMMM Do YYYY")); 
 })
 
